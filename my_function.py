70ac9d519a53d5c448872874cda2e9b9ea463170
nicolasrequin <nicolas.requin@etu.univ-tlse2.fr>
2025-12-21 14:44:51 +0000

    écriture de my_function

--- my_function.py
+++ my_function.py
@@ -1,21 +1,28 @@
 import os
 
 
-def rasterization(in_vector, out_image, field_name, dtype=None):
+def rasterization(in_vector, out_image, field_name, sptial_resolution):
     """
-    See otbcli_rasterisation for details on parameters
+    Rasterisation d'un shapefile en appelant la fonction gdal_rasterize
+
+    Paramètres:
+    -------------
+        in_vector (str): Shapefile à rasteriser
+        out_image (str): Chemin d'accès et nom du raster de sortie
+        field_name (str): Nom du champ de référence pour la rasterisation
+        sptial_resolution (int): Résolution spatiale du raster de sortie
     """
 
-    # define command pattern to fill with paremeters
+    # Raterisation du shapefile
     cmd_pattern = ("gdal_rasterize -a {field_name} "
-                "-tr {sptial_resolution} {sptial_resolution} "
-                "-te {xmin} {ymin} {xmax} {ymax} -ot Byte -of GTiff "
-                "{in_vector} {out_image}")
+                   "-tr {sptial_resolution} {sptial_resolution} "
+                   "-ot Byte -of GTiff "
+                   "{in_vector} {out_image} "
+                   "-a_nodata 0")
 
     # fill the string with the parameter thanks to format function
-    cmd = cmd_pattern.format(in_vector=in_vector, xmin=xmin, ymin=ymin, xmax=xmax,
-                            ymax=ymax, out_image=out_image, field_name=field_name,
-                            sptial_resolution=sptial_resolution)
+    cmd = cmd_pattern.format(in_vector=in_vector, out_image=out_image,
+                             field_name=field_name, sptial_resolution=sptial_resolution)
 
     # execute the command in the terminal
     os.system(cmd)
