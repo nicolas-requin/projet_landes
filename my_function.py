38a1314be7154287ef6f57bff2f2a99f603ecf3e
nicolasrequin <nicolas.requin@etu.univ-tlse2.fr>
2026-01-24 19:02:47 +0000

    création raster id

--- my_function.py
+++ my_function.py
@@ -3,33 +3,6 @@ import numpy as np
 from osgeo import gdal
 
 
-def rasterization(in_vector, out_image, field_name, sptial_resolution):
-    """
-    Rasterisation d'un shapefile en appelant la fonction gdal_rasterize
-
-    Paramètres:
-    -------------
-        in_vector (str): Shapefile à rasteriser
-        out_image (str): Chemin d'accès et nom du raster de sortie
-        field_name (str): Nom du champ de référence pour la rasterisation
-        sptial_resolution (int): Résolution spatiale du raster de sortie
-    """
-
-    # Shapefile rasterization
-    cmd_pattern = ("gdal_rasterize -a {field_name} "
-                   "-tr {sptial_resolution} {sptial_resolution} "
-                   "-ot Byte -of GTiff "
-                   "{in_vector} {out_image} "
-                   "-a_nodata 0")
-
-    # fill the string with the parameter thanks to format function
-    cmd = cmd_pattern.format(in_vector=in_vector, out_image=out_image,
-                             field_name=field_name, sptial_resolution=sptial_resolution)
-
-    # execute the command in the terminal
-    os.system(cmd)
-
-
 def rasterization_from_model(in_vector, out_image, field_name, model_raster):
     """
     Rasterization of a shapefile using the gdal_rasterize
